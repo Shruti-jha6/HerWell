4d550050ee6f7ee4b7f6d7281644c34dd0e3bc5b
Shruti-jha6 <shruti119btcsai22@igdtuw.ac.in>
2025-02-14 23:22:57 +0530

    good

--- frontend/web-app/vite.config.js
+++ frontend/web-app/vite.config.js
@@ -1,8 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(),tailwindcss()],
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(),tailwindcss()],
+//   base: './',
 
-})
+// })
+import imagemin from 'vite-plugin-imagemin';
+
+export default defineConfig({
+  plugins: [
+    imagemin({
+      gifsicle: { optimizationLevel: 7 },
+      optipng: { optimizationLevel: 7 },
+      mozjpeg: { quality: 75 },
+      webp: { quality: 75 }
+    })
+  ]
+});
+
